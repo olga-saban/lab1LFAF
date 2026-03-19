be5a00f50b8e9454378886551a14f8d08b13c960
Olga <olga.saban@isa.utm.md>
2026-03-19 15:24:42 +0200

    lab3

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.List;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one
-        // <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut
-        // actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+        String input = "x = sin(3.14) + cos(2) * 5";
+
+        Lexer lexer = new Lexer(input);
+        List<Token> tokens = lexer.tokenize();
+
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
     }
 }
---
+++ src/TokenType.java
@@ -0,0 +1,24 @@
+public enum TokenType {
+    // Literals
+    INTEGER,
+    FLOAT,
+
+    // Identifiers & keywords
+    IDENTIFIER,
+    SIN,
+    COS,
+
+    // Operators
+    PLUS,
+    MINUS,
+    MUL,
+    DIV,
+    ASSIGN,
+
+    // Parentheses
+    LPAREN,
+    RPAREN,
+
+    // Special
+    EOF
+}
